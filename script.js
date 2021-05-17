d3ef2dfa30c44cf0502c8591f886d730214ff4f4
Gustavo Maldonado <maldonadogustavo55@gmail.com>
2021-05-17 18:52:45 +0200

    Display output work. Miss functionality for more than 2 operands

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 const operand = document.querySelectorAll('.operand');
 const operator = document.querySelectorAll('.operator')
 const equal = document.querySelector('.equal');
+const deleteMemory = document.querySelector('.delete');
 // Display screen: displayInput for entered numbers and displayOperation for current operation
 const displayInput = document.querySelector('#input');
 const displayOperation = document.querySelector('#operation');
 // Variables ti keep track of current operation
-let operation = '';
 let operandOne = '';
 let operandTwo = '';
 let operatorSign = '';
@@ -31,8 +31,10 @@ operand.forEach(function(btn) {
     btn.addEventListener('click', function(e) {
         if(operatorSign === ''){
             operandOne += e.target.textContent;
+            displayInput.textContent = operandOne;
         } else {
             operandTwo += e.target.textContent;
+            displayInput.textContent = operandTwo;
         }
     });
 });
@@ -41,6 +43,7 @@ operand.forEach(function(btn) {
 operator.forEach(function(btn) {
     btn.addEventListener('click', function(e) {
        operatorSign = e.target.textContent;
+       displayOperation.textContent = displayInput.textContent + e.target.textContent;
     });
 });
 
@@ -48,16 +51,28 @@ operator.forEach(function(btn) {
 equal.addEventListener('click', function(e) {
     switch(operatorSign) {
         case '+':
-            result = addNumbers(operandOne, operandTwo);
+            result = addNumbers(Number.parseInt(operandOne), Number.parseInt(operandTwo));
             break;
         case '-':
-            result = substractNumbers(operandOne, operandTwo);
+            result = substractNumbers(Number.parseInt(operandOne), Number.parseInt(operandTwo));
             break;
         case '*':
-            result = multiplyNumbers(operandOne, operandTwo);
+            result = multiplyNumbers(Number.parseInt(operandOne), Number.parseInt(operandTwo));
             break;
         case '/':
-            result = divideNumbers(operandOne, operandTwo);
+            result = divideNumbers(Number.parseInt(operandOne), Number.parseInt(operandTwo));
             break;
     }
+    displayOperation.textContent = operandOne + operatorSign + operandTwo;
+    displayInput.textContent = result;
+});
+
+// Add eventListener() and functionality to delete button
+deleteMemory.addEventListener('click', function(e) {
+    operandOne = '';
+    operandTwo = '';
+    operatorSign = '';
+    result = 0;
+    displayInput.textContent = '';
+    displayOperation.textContent = '';
 });
